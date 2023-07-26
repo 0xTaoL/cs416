d388a9f0f675f48ad4b53ce50d7c52c881eaa3e9
Tao Ling <2415893+0xTaoL@users.noreply.github.com>
2023-07-25 22:29:10 -0400

    update date picker ui

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cs416
 simple data narrative of sp500 performance over the past decade
+
+
+todo
+- add annotations
+- update copy
+- get the date range selectors working
--- constants.js
+++ constants.js
@@ -2,6 +2,16 @@ const svg_width = 1200;
 const svg_height = 600;
 const svg_id = "#sp500-performance-chart";
 
+const _last_10_years_title = "The Last 10 Years";
+
+const _2016_selloff_title = "The 2015-2016 Selloff";
+
+const _2018_crypto_bubble_title = "The 2018 Crypto Bubble";
+
+const _2020_covid_title = "The 2020 COVID-19 Crash";
+
+const _2022_decline_title = "The 2022 Decline";
+
 const _2016_selloff_paragraph =
   "The 2016 selloff was a period of time in which the stock market experienced a sharp decline in value, following the United Kingdom's vote to leave the European Union.";
 
@@ -16,13 +26,3 @@ const _2022_decline_paragraph =
 
 const _last_10_years_paragraph =
   "The last 10 years have been a period of time in which the stock market experienced a sharp increase in value, following the 2008 financial crisis.";
-
-const _last_10_years_title = "The Last 10 Years";
-
-const _2016_selloff_title = "The 2015-2016 Selloff";
-
-const _2018_crypto_bubble_title = "The 2018 Crypto Bubble";
-
-const _2020_covid_title = "The 2020 COVID-19 Crash";
-
-const _2022_decline_title = "The 2022 Decline";
