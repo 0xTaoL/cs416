175dd12118c3308e38938ce11533e684310f84cc
Tao Ling <2415893+0xTaoL@users.noreply.github.com>
2023-07-31 00:15:14 -0400

    update graph desc copy

--- constants.js
+++ constants.js
@@ -6,28 +6,28 @@ const _2016_selloff_graphStart = "2015-06";
 const _2016_selloff_graphEnd = "2016-06";
 const _2016_selloff_title = "The 2015-2016 Selloff";
 const _2016_selloff_paragraph =
-  "The 2016 selloff was a period of time in which the stock market experienced a sharp decline in value, following the United Kingdom's vote to leave the European Union.";
+  "In this visualization, we take a look at several points of interest on the S&P 500 where there was a decline in the price and the possible reasons for the changes. The 2015-2016 selloff was a period of time in which the stock market experienced two major declines in value, but was able to recover relatively quickly. The first decline seems to be related to the stock market popping in China which caused declines globally, the price reached an inflection point in September and was able to recover fully in November. However, the federal reserve and other oil prices dropping seemed to have caused the second decline in value, which was able to recover in March of 2016.";
 
 const _2018_crypto_bubble_graphStart = "2018-08";
 const _2018_crypto_bubble_graphEnd = "2019-01";
 const _2018_crypto_bubble_title = "The 2018 Slowdown";
 const _2018_crypto_bubble_paragraph =
-  "The S&P 500 dropped in late 2018 due to a combination of factors, including the Federal Reserve raising interest rates, the U.S.-China trade war, and a global economic slowdown.";
+  "The S&P 500 dropped in late 2018 due to a combination of factors. One of the major factors was the Trump administration's tariffs on imported aluminum, steel, and other goods, which introduced a large amount of uncertainty into the global economy. Additionally, the Federal Reserve raised interest rates four times in 2018, leading to concerns that the central bank was raising rates too quickly. Concerns over subdued earnings and regulatory fears led to a sell-off of large tech companies. There were also concerns that company earnings were inflated, which may have contributed to the decline in the stock market. The GOP tax cuts may have also played a role in the decline of the stock market. Despite signs that the general economy was still doing well, with record low unemployment, strong GDP growth, and relatively low inflation, other factors outweighed these positive economic indicators.";
 
 const _2020_covid_graphStart = "2020-01";
 const _2020_covid_graphEnd = "2020-08";
 const _2020_covid_title = "The 2020 COVID-19 Crash";
 const _2020_covid_paragraph =
-  "The 2020 COVID-19 crash was a period of time in which the stock market experienced a sharp decline in value, following the COVID-19 pandemic.";
+  "The 2020 crash occurred because investors were worried about the impact of the COVID-19 coronavirus pandemic. The uncertainty over the danger of the virus, plus the shuttering of many businesses and industries as states implemented shutdown orders, damaged many sectors of the economy. The drop was caused by unbridled global fears about the spread of the coronavirus, oil price drops, and the possibility of a 2020 recession. Although the 2020 market crash was dramatic, it didn't last. The stock market experienced a surprising recovery, even as many areas of the U.S. economy continued to experience trouble";
 
 const _2022_decline_graphStart = "2021-12";
 const _2022_decline_graphEnd = "2023-02";
 const _2022_decline_title = "The 2022 Decline";
 const _2022_decline_paragraph =
-  "The 2022 decline was a period of time in which the stock market experienced a  decline in value, following an inflation surge and global supply chain crisis.";
+  "In 2022, the S&P 500 experienced a decline due to several events and factors. High inflation, high interest rates, and growing concerns of a recession all weighed on markets. Investors have been rocked by growing concerns about an economic slowdown caused by surging inflation, with the Federal Reserve scrambling to raise interest rates and tighten monetary policy in response.";
 
 const _last_10_years_graphStart = "2011-01";
 const _last_10_years_graphEnd = "2023-01";
 const _last_10_years_title = "The Last 10 Years";
 const _last_10_years_paragraph =
-  "The last 10 years have been a period of time in which the stock market experienced a sharp increase in value, following the 2008 financial crisis.";
+  "Over the past decade, the S&P 500 has experienced both a phenomenal bull run, as well as significant volatility. The index is still down nearly 14% from its peak in January 2022 and has been in and out of bear market territory over the past year. Despite this, the S&P 500 has had an impressive performance over the past decade, with an average annual return of 13.6%. This is due in part to the strong performance of the US economy, as well as the actions of the Federal Reserve to support the markets.";
